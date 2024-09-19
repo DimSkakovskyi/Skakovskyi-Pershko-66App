be4a70df8a4d4330c1a74df2c1bdbdc3518facba
DimSkakovskyi <dimonskak@ukr.net>
2024-09-19 22:38:01 +0300

    List code example1

--- index.ts
+++ index.ts
@@ -1,3 +1,41 @@
-console.log("Hello world!");
+import inquirer from "inquirer";
 
-let smt = "Gary";
+let todos: string[] = [];
+let loop = true;
+
+while(loop)
+{
+    const answers: {
+        TODO: string,
+        addMore: boolean
+    } = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in yout To-do list?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add it to your list?"
+        },
+    ])
+
+    const{ TODO, addMore } = answers;
+
+    loop = addMore;
+    if(TODO){
+        todos.push(TODO);
+    } else {
+        console.log("Add valid input");
+    }
+}
+if(todos.length > 0){
+    console.log("Your To-do list is: ");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No tsaks found");
+}
