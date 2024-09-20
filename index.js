d6773d76561f7b5067aaf045f3ad5df817c265cb
DimSkakovskyi <dimonskak@ukr.net>
2024-09-20 21:39:06 +0300

    Working example
    Didn't change much after last commit. Only tried to compile it

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in yout To-do list?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add it to your list?"
+        },
+    ]);
+    const { TODO, addMore } = answers;
+    loop = addMore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your To-do list is: ");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No tsaks found");
+}
