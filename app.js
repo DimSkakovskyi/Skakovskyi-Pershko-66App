2804fcddbf03cb7fa9de349025e7dcb6da3f4c00
Svyatoy1 <svyatoslavpershko@gmail.com>
2024-09-29 17:57:18 +0300

    updated app.js

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ import { AppDataSource } from './config/ormconfig';
 
 const app = express();
 
-// Підключення до бази даних
 AppDataSource.initialize()
   .then(() => {
     console.log('Database connected');
   })
   .catch((error) => console.log('Database connection error:', error));
 
-// Middleware та інші налаштування
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
