3810e98e58f8f0e39c1d4989b8b561ec5869f900
DimSkakovskyi <dimonskak@ukr.net>
2024-11-24 14:28:52 +0200

    Testing database connection

--- server/config/knexfile.js
+++ server/config/knexfile.js
@@ -6,7 +6,7 @@ const knexConfig = {
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      password: String(process.env.DB_PASSWORD),
       database: process.env.DB_NAME,
       port: process.env.DB_PORT,
     },
--- server/config/ormconfig.js
+++ server/config/ormconfig.js
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  password: String(process.env.DB_PASSWORD),
   database: process.env.DB_DATABASE,
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
